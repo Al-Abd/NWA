a2a5f584a6f01759d63aad96487dce08ab0d1920
nema <40147733+nexnema@users.noreply.github.com>
2021-11-22 21:15:06 +0330

    0.1.0

--- javascript/setting.js
+++ javascript/setting.js
@@ -5,20 +5,20 @@
  * @returns Void
  */
 function nwaSettingConfigOnLoad() {
-    if (!localStorage.getItem('nwaLang'))
-        if ($nwaConfig.supportedLanguages.includes(window.navigator.language))
-            localStorage.setItem('nwaLang', window.navigator.language);
-        else localStorage.setItem('nwaLang', 'enUS');
-    if (!localStorage.getItem('nwaZoom'))
-        localStorage.setItem('nwaZoom', '100');
-    if (!localStorage.getItem('nwaTheme'))
-        localStorage.setItem('nwaTheme', 'system');
-    if (!localStorage.getItem('nwaColor'))
-        localStorage.setItem('nwaColor', $nwaConfig.defaultColor);
-    nwaLangChange();
-    nwaZoomChange();
-    nwaChangeTheme();
-    nwaChangeColor();
+	if (!localStorage.getItem('nwaLang'))
+		if ($nwaConfig.supportedLanguages.includes(window.navigator.language))
+			localStorage.setItem('nwaLang', window.navigator.language);
+		else localStorage.setItem('nwaLang', 'enUS');
+	if (!localStorage.getItem('nwaZoom'))
+		localStorage.setItem('nwaZoom', '100');
+	if (!localStorage.getItem('nwaTheme'))
+		localStorage.setItem('nwaTheme', 'system');
+	if (!localStorage.getItem('nwaColor'))
+		localStorage.setItem('nwaColor', $nwaConfig.defaultColor);
+	nwaLangChange();
+	nwaZoomChange();
+	nwaChangeTheme();
+	nwaChangeColor();
 }
 
 /**
@@ -28,14 +28,14 @@ function nwaSettingConfigOnLoad() {
  * @returns Void
  */
 function nwaNavSettingChangeThemeOnClick() {
-    if (nwaSelect('html').classList.contains('nwaThemeLight')) {
-        localStorage.setItem('nwaTheme', 'system');
-    } else if (nwaSelect('html').classList.contains('nwaThemeDark')) {
-        localStorage.setItem('nwaTheme', 'nwaThemeLight');
-    } else {
-        localStorage.setItem('nwaTheme', 'nwaThemeDark');
-    }
-    nwaChangeTheme();
+	if (nwaSelect('html').classList.contains('nwaThemeLight')) {
+		localStorage.setItem('nwaTheme', 'system');
+	} else if (nwaSelect('html').classList.contains('nwaThemeDark')) {
+		localStorage.setItem('nwaTheme', 'nwaThemeLight');
+	} else {
+		localStorage.setItem('nwaTheme', 'nwaThemeDark');
+	}
+	nwaChangeTheme();
 }
 
 /**
@@ -45,10 +45,10 @@ function nwaNavSettingChangeThemeOnClick() {
  * @returns Void
  */
 function nwaChangeTheme() {
-    nwaSelect('html').classList.remove('nwaThemeDark');
-    nwaSelect('html').classList.remove('nwaThemeLight');
-    if (localStorage.getItem('nwaTheme') != 'system')
-        nwaSelect('html').classList.add(localStorage.getItem('nwaTheme'));
+	nwaSelect('html').classList.remove('nwaThemeDark');
+	nwaSelect('html').classList.remove('nwaThemeLight');
+	if (localStorage.getItem('nwaTheme') != 'system')
+		nwaSelect('html').classList.add(localStorage.getItem('nwaTheme'));
 }
 
 /**
@@ -58,16 +58,16 @@ function nwaChangeTheme() {
  * @returns Void
  */
 function nwaNavSettingChangeColorOnClick() {
-    localStorage.setItem('nwaTheme', 'nwaThemeLight');
-    if (nwaSelect('html').classList.contains('nwaColorPurpleGreen')) {
-        localStorage.setItem('nwaColor', 'default');
-    } else if (nwaSelect('html').classList.contains('nwaColorBlackGold')) {
-        localStorage.setItem('nwaColor', 'nwaColorPurpleGreen');
-    } else {
-        localStorage.setItem('nwaColor', 'nwaColorBlackGold');
-    }
-    nwaChangeTheme();
-    nwaChangeColor();
+	localStorage.setItem('nwaTheme', 'nwaThemeLight');
+	if (nwaSelect('html').classList.contains('nwaColorPurpleGreen')) {
+		localStorage.setItem('nwaColor', 'default');
+	} else if (nwaSelect('html').classList.contains('nwaColorBlackGold')) {
+		localStorage.setItem('nwaColor', 'nwaColorPurpleGreen');
+	} else {
+		localStorage.setItem('nwaColor', 'nwaColorBlackGold');
+	}
+	nwaChangeTheme();
+	nwaChangeColor();
 }
 
 /**
@@ -76,10 +76,10 @@ function nwaNavSettingChangeColorOnClick() {
  * @returns Void
  */
 function nwaChangeColor() {
-    nwaSelect('html').classList.remove('nwaColorBlackGold');
-    nwaSelect('html').classList.remove('nwaColorPurpleGreen');
-    if (localStorage.getItem('nwaColor') != 'default')
-        nwaSelect('html').classList.add(localStorage.getItem('nwaColor'));
+	nwaSelect('html').classList.remove('nwaColorBlackGold');
+	nwaSelect('html').classList.remove('nwaColorPurpleGreen');
+	if (localStorage.getItem('nwaColor') != 'default')
+		nwaSelect('html').classList.add(localStorage.getItem('nwaColor'));
 }
 
 /**
@@ -90,11 +90,11 @@ function nwaChangeColor() {
  * @returns Void
  */
 function nwaSettingSelectOnChangeEvent(selectName) {
-    var selectSelector = nwaSelect('nav select[name="" + selectName + ""]');
-    var selectValue =
-        selectSelector.options[selectSelector.selectedIndex].value;
-    localStorage.setItem(selectName, selectValue);
-    window[selectName + 'Change']();
+	var selectSelector = nwaSelect('nav select[name="" + selectName + ""]');
+	var selectValue =
+		selectSelector.options[selectSelector.selectedIndex].value;
+	localStorage.setItem(selectName, selectValue);
+	window[selectName + 'Change']();
 }
 
 /**
@@ -103,10 +103,10 @@ function nwaSettingSelectOnChangeEvent(selectName) {
  * @returns Void
  */
 function nwaSetDirToHtmlTag() {
-    const rtlLangList = ['ar', 'fa'];
-    if (rtlLangList.includes(localStorage.getItem('nwaLang')))
-        nwaSelect('html').setAttribute('dir', 'rtl');
-    else nwaSelect('html').setAttribute('dir', 'ltr');
+	const rtlLangList = ['ar', 'fa'];
+	if (rtlLangList.includes(localStorage.getItem('nwaLang')))
+		nwaSelect('html').setAttribute('dir', 'rtl');
+	else nwaSelect('html').setAttribute('dir', 'ltr');
 }
 
 /**
@@ -115,23 +115,23 @@ function nwaSetDirToHtmlTag() {
  * @returns Void
  */
 function nwaLangChange() {
-    nwaSelect('nav select[name="nwaLang"]').value =
-        localStorage.getItem('nwaLang');
+	nwaSelect('nav select[name="nwaLang"]').value =
+		localStorage.getItem('nwaLang');
 
-    //apiUrl cant set in NWA system and must be set in users code.
-    nwaAjax({
-        method: 'GET',
-        url: `${apiUrl}/nwaLanguage/${localStorage.getItem('nwaLang')}`,
-        success: function (status, responseText) {
-            localStorage.setItem('translate', responseText);
-            console.log('nwa Lang is: ' + localStorage.getItem('nwaLang'));
-            nwaSetDirToHtmlTag();
-            nwaLangTranslate();
-        },
-        fail: function (status, responseText) {
-            console.error(status + responseText);
-        },
-    });
+	//apiUrl cant set in NWA system and must be set in users code.
+	nwaAjax({
+		method: 'GET',
+		url: `${apiUrl}/nwaLanguage/${localStorage.getItem('nwaLang')}`,
+		success: function (status, responseText) {
+			localStorage.setItem('translate', responseText);
+			console.log('nwa Lang is: ' + localStorage.getItem('nwaLang'));
+			nwaSetDirToHtmlTag();
+			nwaLangTranslate();
+		},
+		fail: function (status, responseText) {
+			console.error(status + responseText);
+		},
+	});
 }
 
 /**
@@ -140,20 +140,21 @@ function nwaLangChange() {
  * @returns Void
  */
 function nwaLangTranslate() {
-    responseObject = JSON.parse(localStorage.getItem('translate'));
-    // var i = 1;
-    // for (i in responseObject) {
-    //     if (
-    //         responseObject[i].selector != '' &&
-    //         nwaSelect(responseObject[i].selector)
-    //     )
-    //         nwaSelect(responseObject[i].selector).innerHTML =
-    //             responseObject[i].trans + '<br>';
-    // }
-    nwaSelect(responseObject[1].selector).innerHTML =
-        responseObject[1].trans + '<br>';
-    nwaSelect(responseObject[2].selector).innerHTML =
-        responseObject[2].trans + '<br>';
+	responseObject = JSON.parse(localStorage.getItem('translate'));
+	// var i = 1;
+	// for (i in responseObject) {
+	// 	if (
+	// 		responseObject[i].selector != '' &&
+	// 		nwaSelect(responseObject[i].selector)
+	// 	)
+	// 		nwaSelect(responseObject[i].selector).innerHTML =
+	// 			responseObject[i].trans + '<br>';
+	// }
+	responseObject.forEach(myFunction);
+}
+function myFunction(item) {
+	sum += item;
+	nwaSelect(item.selector).innerHTML = item.phrase;
 }
 
 /**
@@ -162,10 +163,10 @@ function nwaLangTranslate() {
  * @returns Void
  */
 function nwaZoomChange() {
-    nwaSelect('nav select[name="nwaZoom"]').value =
-        localStorage.getItem('nwaZoom');
-    nwaSelect('html').style.fontSize =
-        (62.5 / 100) * localStorage.getItem('nwaZoom') + '%';
+	nwaSelect('nav select[name="nwaZoom"]').value =
+		localStorage.getItem('nwaZoom');
+	nwaSelect('html').style.fontSize =
+		(62.5 / 100) * localStorage.getItem('nwaZoom') + '%';
 }
 
 /**
@@ -174,16 +175,16 @@ function nwaZoomChange() {
  * @returns Void
  */
 function nwaChangeVersion() {
-    if (localStorage.getItem('nwaVersion') == 'beta') {
-        nwaSelectVersion('alpha');
-        alert('alpha version activated.');
-    } else if (localStorage.getItem('nwaVersion') == 'alpha') {
-        nwaSelectVersion('default');
-        alert('default version activated.');
-    } else {
-        nwaSelectVersion('beta');
-        alert('beta version activated.');
-    }
+	if (localStorage.getItem('nwaVersion') == 'beta') {
+		nwaSelectVersion('alpha');
+		alert('alpha version activated.');
+	} else if (localStorage.getItem('nwaVersion') == 'alpha') {
+		nwaSelectVersion('default');
+		alert('default version activated.');
+	} else {
+		nwaSelectVersion('beta');
+		alert('beta version activated.');
+	}
 }
 
 /**
@@ -192,20 +193,20 @@ function nwaChangeVersion() {
  * @returns Void
  */
 function nwaSelectVersion(version) {
-    switch (version) {
-        case 'alpha':
-            nwaSelect('body').classList.remove('nwaBeta');
-            nwaSelect('body').classList.add('nwaAlpha');
-            localStorage.setItem('nwaVersion', 'alpha');
-            break;
-        case 'beta':
-            nwaSelect('body').classList.add('nwaBeta');
-            nwaSelect('body').classList.remove('nwaAlpha');
-            localStorage.setItem('nwaVersion', 'beta');
-            break;
-        default:
-            nwaSelect('body').classList.remove('nwaBeta');
-            nwaSelect('body').classList.remove('nwaAlpha');
-            localStorage.removeItem('nwaVersion');
-    }
+	switch (version) {
+		case 'alpha':
+			nwaSelect('body').classList.remove('nwaBeta');
+			nwaSelect('body').classList.add('nwaAlpha');
+			localStorage.setItem('nwaVersion', 'alpha');
+			break;
+		case 'beta':
+			nwaSelect('body').classList.add('nwaBeta');
+			nwaSelect('body').classList.remove('nwaAlpha');
+			localStorage.setItem('nwaVersion', 'beta');
+			break;
+		default:
+			nwaSelect('body').classList.remove('nwaBeta');
+			nwaSelect('body').classList.remove('nwaAlpha');
+			localStorage.removeItem('nwaVersion');
+	}
 }
